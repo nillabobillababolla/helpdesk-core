1788f4ecd27643e231d4ede9f496b64244a28445
Samet Alabey <alabeysamet@gmail.com>
2019-03-27 09:46:29 +0300

    Login,Roles,ViewModels ok.

--- HelpDesk.Web/Controllers/AccountController.cs
+++ HelpDesk.Web/Controllers/AccountController.cs
@@ -1,20 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using HelpDesk.DAL;
+using HelpDesk.Models.Enums;
+using HelpDesk.Models.IdentityEntities;
+using HelpDesk.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace HelpDesk.Web.Controllers
 {
     public class AccountController : Controller
     {
-       public IActionResult Login()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly MyContext _dbContext;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, MyContext dbContext, RoleManager<ApplicationRole> roleManager)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _dbContext = dbContext;
+            _roleManager = roleManager;
         }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
-    }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                Email = model.Email,
+                Name = model.Name,
+                Surname = model.Surname,
+                UserName = model.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await CreateRoles();
+                if (_userManager.Users.Count() == 1)
+                {
+                    await _userManager.AddToRoleAsync(user, IdentityRoles.Admin.ToString());
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, IdentityRoles.Customer.ToString());
+                }
+                return RedirectToAction(nameof(Login));
+            }
+            else
+            {
+                var errMsg = "";
+                foreach (var identityError in result.Errors)
+                {
+                    errMsg += identityError.Description;
+                }
+                ModelState.AddModelError(String.Empty, errMsg);
+                return View(model);
+            }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError(String.Empty, "Kullanıcı adı veya şifre hatalı");
+            return View(model);
+        }
+
+        private async Task CreateRoles()
+        {
+            var roleNames = Enum.GetNames(typeof(IdentityRoles));
+            foreach (var roleName in roleNames)
+            {
+                if (!_roleManager.RoleExistsAsync(roleName).Result)
+                {
+                    await _roleManager.CreateAsync(new ApplicationRole()
+                    {
+                        Name = roleName
+                    });
+                }
+            }
+
+        }
 }
